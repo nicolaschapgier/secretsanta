90c99158aeecb6f9b803eafdc99f177b5b6268b2
Nicolas Chapgier <nicolas.chapgier@sncf.fr>
2024-12-26 10:49:27 +0100

    -

--- secretsanta.py
+++ secretsanta.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 
 def secret_santa(participants, exceptions):
     if (len(participants)) < 2:
@@ -19,22 +20,38 @@ def secret_santa(participants, exceptions):
             return assignments
         
 # Liste des participants
-participants = ["Nicolas", "Audrey", "Thierry", "Annie", "Aurélien", "Marie", "Christophe", "Sylvie", "Laure", "Francis", "Stéphanie"]
+participants = ["Audrey", "Francis", "Nicolas", "Laure", "Annie", "Christophe", "Stéphanie", "Marie", "Thierry", "Sylvie", "Aurélien"]
 
 # Exceptions
 exceptions = {
-    "Nicolas": ["Audrey", "Christophe", "Sylvie","Laure"],
-    "Audrey": ["Nicolas", "Thierry", "Annie","Aurélien"],
-    "Thierry": ["Annie", "Audrey","Aurélien"],
-    "Annie": ["Thierry", "Audrey","Aurélien"],
-    "Aurélien": ["Marie", "Thierry", "Annie", "Audrey"],
+    "Nicolas": ["Audrey", "Christophe", "Sylvie"],
+    "Audrey": ["Nicolas", "Thierry", "Annie"],
+    "Thierry": ["Annie", "Audrey", "Aurélien"],
+    "Annie": ["Thierry", "Audrey", "Aurélien"],
+    "Aurélien": ["Marie", "Thierry", "Annie"],
     "Marie": ["Aurélien","Francis", "Stéphanie"],
-    "Francis": ["Stéphanie","Marie"],
-    "Stéphanie": ["Francis","Marie"],
+    "Francis": ["Stéphanie", "Marie"],
+    "Stéphanie": ["Francis", "Marie"],
     "Christophe": ["Nicolas", "Sylvie", "Laure"],
     "Sylvie": ["Nicolas", "Christophe", "Laure"],
+    "Laure": ["Christophe", "Sylvie"],
 }
 
 assignments = secret_santa(participants, exceptions)
-for giver, receiver in assignments.items():
-    print(f"{giver} donne un cadeau à {receiver}")
+with open("tirage.txt", "a", encoding="UTF-8") as f:
+    current_time = datetime.now().strftime("%d %B %Y à : %Hh:%Mmin:%Ss")
+    f.write(f"\nTirage du {current_time}.\n")
+    count=0
+    for giver, receiver in assignments.items():
+        count+=1
+        f.write(f"{giver} donne un cadeau à {receiver}\n")
+        print("----------------------------------------------")
+        print(f"Tirage {count}/{len(participants)}")
+        print(f"{giver} donne un cadeau à {receiver}")
+        print("Appuie sur entrée pour voir le prochain duo !")
+        print("----------------------------------------------")
+        if count == len(participants):
+            print("Tirage terminé !\n")
+            input()
+            break
+        input()
